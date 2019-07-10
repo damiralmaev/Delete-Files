a151282de61041f5015ad1002c32fa913eb1bfab
damiralmaev <dam.almaev@gmail.com>
2019-07-10 18:25:39 +0300

    1.0
    смотреть релизы)))

---
+++ Delete Files/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Что я добавил
+
+using System.IO;//для управлениями файлами
+using System.Diagnostics;//для управлениями процесамми
+
+namespace Delete_Files
+{
+    public partial class Form1 : Form
+    {
+        //переменные
+
+        public string path = "";//путь
+        public int error = 0;//сколько ошибок?
+        public int deletes;//сколько успешно удалено?
+
+        //разное
+
+        StreamWriter rd = new StreamWriter(@"C:\LogDeleteFiles.txt");//где хранить лог файл
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Btstart_Click(object sender, EventArgs e)
+        {
+            await Work();
+        }
+
+        public async Task Work()
+        {
+            lsLog.Items.Clear();
+            FolderBrowserDialog l = new FolderBrowserDialog();
+            if (l.ShowDialog() == DialogResult.OK)
+            {
+                this.Invoke(new Action(() => { lsLog.Items.Add($"Начало очистки!"); }));
+                rd.WriteLine($"Начало очистки!");
+                path = l.SelectedPath;
+                string[] delete = Directory.GetFiles(path, tbdelete.Text, SearchOption.AllDirectories);
+                this.Invoke(new Action(() => { lsLog.Items.Add($"Всё нашёл! Всего файлов: {delete.Length}"); }));
+                rd.WriteLine($"Всё нашёл! Всего файлов: {delete.Length}");
+                progressDelete.Maximum = delete.Length;
+                //rd.Close();
+                foreach (string name in delete)
+                {
+                    try
+                    {
+                        File.Delete(name);
+                        this.Invoke(new Action(() => { lsLog.Items.Add($"Удалил файл по пути: {name}"); }));
+                        rd.WriteLine($"Удалил файл по пути: {name}");
+                        deletes++;
+                        progressDelete.Value++;
+                        //rd.Close();
+                    }
+                    catch(Exception ex)
+                    {
+                        this.Invoke(new Action(() => { lsLog.Items.Add($"Ошибка удаление по пути: {name}. Ошибка {ex.Message}"); }));
+                        rd.WriteLine($"Ошибка удаление по пути: {name}. Ошибка {ex.Message}");
+                        error++;
+                        progressDelete.Value++;
+                        //rd.Close();
+                    }
+                }
+                this.Invoke(new Action(() => { lsLog.Items.Add($"Очистка завершина! Всего {delete.Length}, успешно {deletes}, ошибка {error}"); }));
+                rd.WriteLine($"Очистка завершина! Всего {delete.Length}, успешно {deletes}, ошибка {error}");
+                //rd.Close();
+                deletes = 0;
+                error = 0;
+                progressDelete.Value = 0;
+                progressDelete.Maximum = 100;
+            }
+            //rd.Close();
+        }
+
+        private void ЗапускToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            btstart.PerformClick();
+        }
+
+        private void ПоказатьЛогиКакФайлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            { Process.Start(@"C:\LogDeleteFiles.txt"); }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"Ошибка открытие логов! Ошибка: {ex.Message}");
+                lsLog.Items.Add($"Ошибка открытие логов! Ошибка: {ex.Message}");
+                rd.WriteLine($"Ошибка открытие логов! Ошибка: {ex.Message}");
+                //rd.Close();
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# Delete-Files
-Это программа (сделано школьником которому 13 лет и на коленки) поможет вам очистить папки от ненужных файлов.
+               d8888b. d88888b db      d88888b d888888b d88888b 
+               88  `8D 88'     88      88'     `~~88~~' 88'     
+               88   88 88ooooo 88      88ooooo    88    88ooooo 
+               88   88 88~~~~~ 88      88~~~~~    88    88~~~~~ 
+               88  .8D 88.     88booo. 88.        88    88.     
+               Y8888D' Y88888P Y88888P Y88888P    YP    Y88888P 
+                                                                
+                                                                
+                   d88888b d888888b db      d88888b .d8888. 
+                   88'       `88'   88      88'     88'  YP 
+                   88ooo      88    88      88ooooo `8bo.   
+                   88~~~      88    88      88~~~~~   `Y8b. 
+                   88        .88.   88booo. 88.     db   8D 
+                   YP      Y888888P Y88888P Y88888P `8888Y' 
+
+***Delete-Files***
+
+# Описание
+
+**Это программа поможет вам очистить папки от ненужных файлов.**
+
+# Зависимости
+
+1. Права `администратора`.
+
+2. `.NET` Framework **4.7.2 и выше**.
+
+# Скриншоты
+
+![](https://psv4.userapi.com/c848124/u455533038/docs/d18/a49d05eff038/Screenshot_2.png?extra=3_cHYYDdLPagy3yoa8Kx2L6uJQ-yw9OodHsKRwaDYc52SOseudo0fP-rFXuVUNCeozClx77u0bLaM0IdfgO-G8Ad96Iu8aN6PXJPz-H6aSVs0MGozdq14wRjHn9AuR5sQGC7CpdcZ--f-yMwwP25DA)
+
+# Автор
+
+Дамир `Алмаев` Маратович
